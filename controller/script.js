db06eb8102cd075e9614b4a9c04c080be6da492b
Samuel Imanol <samuel.justiniani.lancho@vallegrande.edu.pe>
2024-08-02 02:21:34 -0500

    SitioWed

---
+++ controller/script.js
@@ -0,0 +1,11 @@
+function navigate(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+
+function leerMas() {
+    // Aquí va la lógica para leer más información
+}
